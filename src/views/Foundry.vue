755ef4f93ffea78a59f219d43d4e66ea9865ce3e
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-10-08 14:49:28 +0200

    Restored views
    Restored views folder that was removed by accident

---
+++ src/views/Foundry.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="foundry">
+    <h2 class="text-lg m-4">
+      Foundry VTT will open in new tab. If it haven't, you can simply click
+      <br />
+      <a href="http://vtt.fallenempires.eu:30000/join" target="_blank"
+        >THIS LINK</a
+      >
+    </h2>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home grid grid-cols-1 md:grid-cols-3 gap-4">
+    <common-box :content="box1" />
+    <common-box :content="box2" />
+    <common-box :content="box3" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CommonBox, { CommonBoxProps } from '@/components/CommonBox.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    CommonBox
+  },
+  data() {
+    const box1: CommonBoxProps = {
+      title: 'Start here',
+      icon: 'book-open',
+      items: [
+        { label: 'Setting Foundations', route: '/' },
+        { label: 'Planned Campaigns', route: '/' },
+        { label: 'Character Creation', route: '/' },
+        { label: 'Homebrew', route: '/' }
+      ]
+    };
+    const box2: CommonBoxProps = {
+      title: 'Primary ancestries',
+      icon: 'users',
+      items: [
+        { label: 'Human', route: '/' },
+        { label: 'Dwarf', route: '/' },
+        { label: 'Elf', route: '/' }
+      ]
+    };
+    const box3: CommonBoxProps = {
+      title: 'Secondary ancestries',
+      icon: 'user-plus'
+    };
+    return {
+      box1,
+      box2,
+      box3
+    };
+  }
+});
+</script>
