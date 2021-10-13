abeab2cc440c401dbdebb52e632402eea5732566
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-10-13 10:12:28 +0200

    Added Content with Sidebar component
    Added component for displaying content with sidebar

--- src/models/Ancestry.ts
+++ src/models/Ancestry.ts
@@ -1,9 +1,13 @@
 export default interface Ancestry {
   name: string;
+  rarity: string;
   hitPoints: string;
   size: string;
   speed: string;
   abilityBoost: string;
+  languages: string;
   traits?: string[];
   cultures?: string[];
+  startingAncestryFeats?: string[];
+  description: string;
 }
--- src/views/Ancestry.vue
+++ src/views/Ancestry.vue
@@ -1,6 +1,61 @@
 <template>
   <div class="text-left">
     <common-title>{{ ancestry.name }}</common-title>
+    <content-with-sidebar>
+      <template v-slot:content>
+        {{ ancestry.description }}
+      </template>
+      <template v-slot:sidebar>
+        <div class="sidebar">
+          <h3>Rarity</h3>
+          <p class="mb-4">{{ ancestry.rarity }}</p>
+
+          <h3>Hit Points</h3>
+          <p class="mb-4">{{ ancestry.hitPoints }}</p>
+
+          <h3>Size</h3>
+          <p class="mb-4">{{ ancestry.size }}</p>
+
+          <h3>Speed</h3>
+          <p class="mb-4">{{ ancestry.speed }}</p>
+
+          <h3>Ability Boosts</h3>
+          <p class="mb-4">{{ ancestry.abilityBoost }}</p>
+
+          <h3>Languages</h3>
+          <p class="mb-4">
+            {{ ancestry.languages }}
+          </p>
+
+          <h3>Traits</h3>
+          <p class="mb-4">
+            <common-tag
+              v-for="trait in ancestry.traits"
+              v-bind:key="trait.index"
+            >
+              {{ trait }}
+            </common-tag>
+          </p>
+
+          <h3>Cultures</h3>
+          <ul class="mb-4">
+            <li v-for="culture in ancestry.cultures" v-bind:key="culture.index">
+              {{ culture }}
+            </li>
+          </ul>
+
+          <h3>Starting Ancestry Feats</h3>
+          <ul class="mb-4">
+            <li
+              v-for="item in ancestry.startingAncestryFeats"
+              v-bind:key="item.index"
+            >
+              {{ item }}
+            </li>
+          </ul>
+        </div>
+      </template>
+    </content-with-sidebar>
     <common-section-title>Heritages</common-section-title>
     <div class="home grid grid-cols-1 md:grid-cols-2 gap-4">
       <heritage-box
@@ -25,6 +80,8 @@ import Ancestry from '@/models/Ancestry';
 import Feat from '@/models/Feat';
 import Heritage from '@/models/Heritage';
 import CommonSectionTitle from '@/components/CommonSectionTitle.vue';
+import ContentWithSidebar from '@/components/ContentWithSidebar.vue';
+import CommonTag from '@/components/CommonTag.vue';
 
 export default defineComponent({
   name: 'Ancestry',
@@ -32,7 +89,9 @@ export default defineComponent({
     CommonTitle,
     FeatBox,
     HeritageBox,
-    CommonSectionTitle
+    CommonSectionTitle,
+    ContentWithSidebar,
+    CommonTag
   },
   data() {
     return {
@@ -60,3 +119,11 @@ export default defineComponent({
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.sidebar h3 {
+  font-family: 'Teko', sans-serif;
+  font-size: 1.5rem;
+  font-weight: 400;
+}
+</style>
