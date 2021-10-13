5f1388e70ad0570e821230e5303bf2c2fef10914
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-10-13 10:17:49 +0200

    Added Content with Sidebar component (#7)
    Added component for displaying content with sidebar

---
+++ src/components/ContentWithSidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home grid grid-cols-1 md:grid-cols-4 gap-4">
+    <div class="col-span-3 p-2"><slot name="content"></slot></div>
+    <div class="border-l border-red-900 p-2"><slot name="sidebar"></slot></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ContentWithSidebar'
+});
+</script>
