177887c3a11a4185179d31600edc36d964c347f6
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-10-15 12:59:30 +0200

    Created server.js entrypoint

---
+++ server.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middleware);
+server.use(router);
+
+server.listen(port);
