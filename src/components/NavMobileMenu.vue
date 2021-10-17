73e6cdf1a28df8d33cea37f060d2825570a39c7c
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-10-17 23:50:40 +0200

    Added mobile menu

---
+++ src/components/NavMobileMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="px-2 py-3 space-y-1">
+    <nav-mobile-menu-item
+      v-for="item in menuItems"
+      :key="item.caption"
+      :label="item.label"
+      :route="item.route"
+      :icon="item.icon"
+      :subItems="item.subItems"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import NavMobileMenuItem from '@/components/NavMobileMenuItem.vue';
+import Navigation from '@/models/Navigation';
+
+export default defineComponent({
+  name: 'NavMobileMenu',
+  components: {
+    NavMobileMenuItem
+  },
+  props: {
+    menuItems: {
+      type: Object as PropType<Navigation>,
+      required: true
+    }
+  }
+});
+</script>
