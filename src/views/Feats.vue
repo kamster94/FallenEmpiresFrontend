dcf202d7478104e79d1315fcfe5054515d827372
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-10-18 23:39:24 +0200

    Prepared Feats and Heritages views

--- src/views/Feats.vue
+++ src/views/Feats.vue
@@ -1,15 +1,31 @@
 <template>
-  <div><common-title>Feats</common-title></div>
+  <router-view v-if="this.$route.path.match('/feats/+')" />
+  <div v-else>
+    <common-title>Feats</common-title>
+    <common-table :table="feats" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import CommonTitle from '@/components/CommonTitle.vue';
+import CommonTable from '@/components/CommonTable.vue';
+import Table from '@/models/Table';
 
 export default defineComponent({
   name: 'Feats',
   components: {
-    CommonTitle
+    CommonTitle,
+    CommonTable
+  },
+  data() {
+    const feats: Table = {
+      headers: ['Name', 'Level', 'Traits'],
+      rows: []
+    };
+    return {
+      feats
+    };
   }
 });
 </script>
--- src/views/Heritages.vue
+++ src/views/Heritages.vue
@@ -1,15 +1,31 @@
 <template>
-  <div><common-title>Heritages</common-title></div>
+  <router-view v-if="this.$route.path.match('/heritages/+')" />
+  <div v-else>
+    <common-title>Heritages</common-title>
+    <common-table :table="heritages" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import CommonTitle from '@/components/CommonTitle.vue';
+import CommonTable from '@/components/CommonTable.vue';
+import Table from '@/models/Table';
 
 export default defineComponent({
   name: 'Heritages',
   components: {
-    CommonTitle
+    CommonTitle,
+    CommonTable
+  },
+  data() {
+    const heritages: Table = {
+      headers: ['Name', 'Ancestries', 'Value'],
+      rows: []
+    };
+    return {
+      heritages
+    };
   }
 });
 </script>
