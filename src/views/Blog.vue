5dcd9953715eab66dc075a6a19014b7d7c3dc1e7
Kamil Chmielewski <kamster94@users.noreply.github.com>
2021-11-09 23:26:20 +0100

    Added blog display - Created component displaying blog post - Used it in blog view - Created blog data service - Added some posts to db.json

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,16 +1,35 @@
 <template>
   <common-title>Blog</common-title>
+  <blog-post
+    v-for="post in posts"
+    v-bind:key="post.id"
+    :title="post.title"
+    :author="post.author"
+    :date="new Date(post.date)"
+    :content="post.content"
+  />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import BlogDataService from '@/services/BlogDataService';
+
 import CommonTitle from '@/components/CommonTitle.vue';
+import BlogPost from '@/components/BlogPost.vue';
 
 export default defineComponent({
   name: 'Blog',
   components: {
-    CommonTitle
+    CommonTitle,
+    BlogPost
+  },
+  async setup() {
+    const posts = await BlogDataService.get();
+
+    return {
+      posts
+    };
   }
 });
 </script>
